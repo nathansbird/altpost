da2be895161896fe28eb123e8b6d129d93988575
Nathan Bird <chosenbird1@gmail.com>
2024-09-18 13:45:59 -0500

    Add pricing

---
+++ src/app/payment/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{width: "100%", padding: "100px 0px"}}>
+      <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
+      <div dangerouslySetInnerHTML={{__html: '<stripe-pricing-table pricing-table-id="prctbl_1Q0SbhGTdwHuL5QiHH6g5oI6" publishable-key="pk_live_51K7rvbGTdwHuL5QieweNfzjnF5h458ZEBuaEH6TWdhlGGkPxQDkR13N3kgU4YLZ5KFT0r0r1OMFFSrMrZUg55ydW00oOWw1Eeb"></stripe-pricing-table>'}}></div>
+    </div>
+  )
+}
